9b6e70bb7f33944ea7ec37c6f6bc4b614fce4f87
Richard Nguyen <richardnguyenee@gmail.com>
2016-09-05 22:52:53 -0400

    fixed RoverCommand.cpp

--- libraries/RoverCommand/RoverCommand.cpp
+++ libraries/RoverCommand/RoverCommand.cpp
@@ -39,7 +39,7 @@ void RoverCommand::reset()
 void RoverCommand::parseCommand(String dataString)
 {
 		//WRITE ME LATER
-		dataString.
+		
 		//take the dataString and parse it to grab only the rover command
 		//no need to check for validity as it's different for every Arduino. So instead check it at the Ardunio's ino
 		//this->_roverCommand = "cmd0";//DEBUG
