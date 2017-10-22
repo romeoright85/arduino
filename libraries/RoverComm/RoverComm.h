a843b24ed47bf1ec6437ad825a5486363c6b6f7e
Richard Nguyen <richardnguyenee@gmail.com>
2017-10-22 19:16:31 -0400

    updated code

--- libraries/RoverComm/RoverComm.h
+++ libraries/RoverComm/RoverComm.h
@@ -21,13 +21,13 @@
 
 
 
-		==Rover Data Format==
+	==Rover Data Format==
 		The format is /OcDLL*tttdddd... (Note: The d's can range from none to up to _MAX_ROVER_COMMAND_DATA_LEN_ - 1, i.e. 15-1=14. Minus 1 due to null character. So hence dddd....)
 		The /, c, and * are format identifiers. For commands the headers are as such: /-c---*----
 		The O shows the origin where the message came from (Origin ID): See definitions below.
 		The D shows the destination Arduino it's going to (Destination ID): See definitions below.
 		Definitions:
-			0=NONE, 1=CMNC (external), 2=NAVI (internal), 3=AUXI (internal), 4=MAIN (internal), 5=COMM (internal)
+			0=NONE, 1=CMNC (external), 2=NAVI (internal), 3=AUXI (internal), 4=MAIN (internal), 5=COMM (internal), 6=PC_USB(internal)
 		The LL shows command priority level it should be in the lookup table
 		The ttt are the rover command tag to represent which command it is.
 		The d's (none to _MAX_ROVER_COMMAND_DATA_LEN_ - 1, i.e. 15-1 = 14. Minus 1 due to null character.) are the letters that designate the command data, if any.
@@ -35,6 +35,8 @@
 		
 		(where - are don't cares and ... means it can vary in length)
 		Any messages for
+			NONE: 
+				/-c0--*----.... 
 			CMNC: 
 				/-c1--*----.... 
 			NAVI:
@@ -45,10 +47,12 @@
 				/-c4--*----.... 				
 			COMM:
 				/-c5--*----.... 
-				
+			PC_USB:
+				/-c6--*----.... 			
 				
 	Example:
 		/3c101*002HelloMAINtoCMNC
+		
 		
 */	
 
